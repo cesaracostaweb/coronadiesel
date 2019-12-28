27f4f623f7a7d4bedd6653061a6b7d4e3bb274bd
cesaracostaweb <acostarcesar@gmail.com>
2019-12-28 00:52:26 -0300

    servicios changed

--- src/assets/data/articulos.js
+++ src/assets/data/articulos.js
@@ -1,36 +1,80 @@
 
 const articulos = [
     {
-        "nombre": "Inyectora",
-        "imagen": "../../assets/img/1 (1).jpeg"
+        "nombre": "Reforma Kangoo 1.9 Bosch",
+        "imagen": "../../assets/img/1.jpeg"
     },
     {
-        "nombre": "Combos Luminosos",
-        "imagen": "../../assets/img/1 (2).jpeg"
+        "nombre": "Vw polo /Caddy /Saveiro /gol 1.9",
+        "imagen": "../../assets/img/2.jpeg"
     },
     {
-        "nombre": "Combos Mix",
-        "imagen": "../../assets/img/1 (3).jpeg"
+        "nombre": "Peugot partner /206 Lucas /delphi",
+        "imagen": "../../assets/img/3.jpeg"
     },
     {
-        "nombre": "Combos Livings con Butacones",
-        "imagen": "../../assets/img/1 (4).jpeg"
+        "nombre": "Renault 1.9 Kangoo / clio /express",
+        "imagen": "../../assets/img/4.jpeg"
     },
     {
-        "nombre": "Combos Livings con Ecocuero",
-        "imagen": "../../assets/img/1 (5).jpeg"
+        "nombre": "Inyectores Clio /Kangoo / logan /symbol dci 1.5",
+        "imagen": "../../assets/img/5.jpeg"
     },
     {
-        "nombre": "Combos Princesa",
-        "imagen": "../../assets/img/1 (6).jpeg"
+        "nombre": "Inyectores Clio /Kangoo / logan /symbol dci 1.5",
+        "imagen": "../../assets/img/6.jpeg"
     },
     {
-        "nombre": "Combos Princesa",
-        "imagen": "../../assets/img/1 (7).jpeg"
+        "nombre": "Reforma Ford Focus 1.8 / reforma Chevrolet Astra 2.0",
+        "imagen": "../../assets/img/7.jpeg"
     },
     {
-        "nombre": "Combos Mesas Altas",
-        "imagen": "../../assets/img/1 (8).jpeg"
+        "nombre": "Reforma Ford Focus 1.8 / reforma Chevrolet Astra 2.0",
+        "imagen": "../../assets/img/8.jpeg"
+    },
+    {
+        "nombre": "Bomba de alta Clio /Kangoo/ logan /symbol dci 1.5",
+        "imagen": "../../assets/img/9.jpeg"
+    },
+    {
+        "nombre": "Bomba de alta Clio /Kangoo/ logan /symbol dci 1.5",
+        "imagen": "../../assets/img/10.jpeg"
+    },
+    {
+        "nombre": "Vw senda 1.6 Bosch",
+        "imagen": "../../assets/img/11.jpeg"
+    },
+    {
+        "nombre": "Vw senda 1.6 Bosch",
+        "imagen": "../../assets/img/12.jpeg"
+    },
+    {
+        "nombre": "Vw senda 1.6 Bosch",
+        "imagen": "../../assets/img/13.jpeg"
+    },
+    {
+        "nombre": "Vw senda 1.6 Bosch",
+        "imagen": "../../assets/img/14.jpeg"
+    },
+    {
+        "nombre": "Bomba de alta presión peugot partner 1.6 hdi",
+        "imagen": "../../assets/img/15.jpeg"
+    },
+    {
+        "nombre": "Bomba de alta presión peugot partner 1.6 hdi",
+        "imagen": "../../assets/img/16.jpeg"
+    },
+    {
+        "nombre": "Bomba de alta presión peugot partner 1.6 hdi",
+        "imagen": "../../assets/img/17.jpeg"
+    },
+    {
+        "nombre": "Bomba de alta presión peugot partner 1.6 hdi",
+        "imagen": "../../assets/img/18.jpeg"
+    },
+    {
+        "nombre": "Bomba de alta presión peugot partner 1.6 hdi",
+        "imagen": "../../assets/img/19.jpeg"
     },
 
     
